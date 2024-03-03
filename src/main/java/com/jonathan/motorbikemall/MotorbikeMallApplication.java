962861b4a780f3e68abb1fb22e9679baa3d5db5f
Jonathan <jonahtan876200@gmail.com>
2024-03-03 16:08:23 +0800

    initial springboot project

---
+++ src/main/java/com/jonathan/motorbikemall/MotorbikeMallApplication.java
@@ -0,0 +1,13 @@
+package com.jonathan.motorbikemall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorbikeMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotorbikeMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jonathan/motorbikemall/MotorbikeMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathan.motorbikemall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorbikeMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
